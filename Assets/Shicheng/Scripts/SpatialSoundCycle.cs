7261c8c3ce5c6890a980730bee22bb16ba4aff28
hsc <smsmun.husc@outlook.com>
2026-09-10 17:23:43 -0500

    PE triggered

--- Assets/Shicheng/Scripts/SpatialSoundCycle.cs
+++ Assets/Shicheng/Scripts/SpatialSoundCycle.cs
@@ -4,50 +4,75 @@ using UnityEngine.InputSystem;
 public class SpatialSoundCycle : MonoBehaviour
 {
     public InputActionReference playNextAction;
+
     public AudioSource[] audioSources;
+    public ParticleSystem[] particleSystems;
 
     private int currentIndex = 0;
 
     private void OnEnable()
     {
         playNextAction.action.Enable();
-        playNextAction.action.performed += PlayNextSound;
+        playNextAction.action.performed += PlayNextEffect;
     }
 
     private void OnDisable()
     {
-        playNextAction.action.performed -= PlayNextSound;
+        playNextAction.action.performed -= PlayNextEffect;
         playNextAction.action.Disable();
     }
 
-    private void PlayNextSound(InputAction.CallbackContext context)
+    private void PlayNextEffect(InputAction.CallbackContext context)
     {
         if (audioSources == null || audioSources.Length == 0)
             return;
 
+        // Stop all sounds
         foreach (AudioSource source in audioSources)
         {
             if (source != null)
                 source.Stop();
         }
 
+        // Stop all particle effects
+        foreach (ParticleSystem particle in particleSystems)
+        {
+            if (particle != null)
+            {
+                particle.Stop();
+                particle.Clear();
+            }
+        }
+
+        // Play current sound
         AudioSource currentSource = audioSources[currentIndex];
 
         if (currentSource != null)
         {
             currentSource.Play();
-
-            Debug.Log(
-                "Playing Spatial Sound " +
-                (currentIndex + 1) +
-                "/" +
-                audioSources.Length +
-                ": " +
-                currentSource.gameObject.name
-            );
         }
 
+        // Play current particle effect
+        if (particleSystems != null &&
+            currentIndex < particleSystems.Length)
+        {
+            ParticleSystem currentParticle = particleSystems[currentIndex];
+
+            if (currentParticle != null)
+            {
+                currentParticle.Play();
+            }
+        }
+
+        Debug.Log(
+            "Playing Effect " +
+            (currentIndex + 1) +
+            "/" +
+            audioSources.Length
+        );
+
         currentIndex++;
+
         if (currentIndex >= audioSources.Length)
             currentIndex = 0;
     }
